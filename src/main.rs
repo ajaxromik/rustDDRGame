5415f7086f95a31472aac68a1e51f881bf3c29ea
aromik <awillcarr@gmail.com>
2024-10-28 13:27:17 -0400

    added an image

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 // Open a window, and draw a colored square in it
 use quicksilver::{
     geom::{Rectangle, Vector},
-    graphics::Color,
+    graphics::{Color, Image},
     run, Graphics, Input, Result, Settings, Window,
 };
 
 fn main() {
     run(
         Settings {
-            title: "Square Example",
+            title: "Canvas Painter",
             ..Settings::default()
         },
         app,
@@ -21,11 +21,17 @@ async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> 
     gfx.clear(Color::WHITE);
     // Paint a blue square with a red outline in the center of our screen
     // It should have a top-left of (350, 100) and a size of (150, 100)
+    let image = Image::load(&gfx, "thwomp.png").await?;
+    let image_rect = Rectangle::new(Vector::new(80.0,120.0), image.size());
+    gfx.draw_image(&image, image_rect);
+    gfx.present(&window)?;
+
     let rect = Rectangle::new(Vector::new(350.0, 100.0), Vector::new(100.0, 100.0));
     gfx.fill_rect(&rect, Color::BLUE);
     gfx.stroke_rect(&rect, Color::RED);
     // Send the data to be drawn
     gfx.present(&window)?;
+
     loop {
         while let Some(_) = input.next_event().await {}
     }
